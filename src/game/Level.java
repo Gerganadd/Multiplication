a051c1135257daf75f5981e16f7fdbdb1384bb36
Gerganadd <gerganadoncheva2003@gmail.com>
2022-02-12 21:13:45 +0200

    created level class

--- src/game/Level.java
+++ src/game/Level.java
@@ -1,5 +1,43 @@
 package game;
 
-public class Level {
+import java.util.Map;
 
+public class Level
+{
+	private String backgroundPicturePath;
+	private int taskPixels;
+	private Map<Integer, Coordinate> tasksCoordinates;
+	
+	public Level(String backgroundPicturePath, int taskPixels, Map<Integer, Coordinate> tasksCoordinates)
+	{
+		if (backgroundPicturePath != null && !backgroundPicturePath.isBlank())
+		{
+			this.backgroundPicturePath = backgroundPicturePath;
+		}
+		
+		if (taskPixels > 0)
+		{
+			this.taskPixels = taskPixels;
+		}
+		
+		if(tasksCoordinates != null)
+		{
+			this.tasksCoordinates = tasksCoordinates;
+		}
+	}
+	
+	public String getBackgroundPicturePath()
+	{
+		return this.backgroundPicturePath;
+	}
+	
+	public int getTaskPixels()
+	{
+		return this.taskPixels;
+	}
+	
+	public Map<Integer, Coordinate> getCordinates()
+	{
+		return this.tasksCoordinates;
+	}
 }
